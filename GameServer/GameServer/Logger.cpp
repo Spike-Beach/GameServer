3cc0eb1486edef46d297eae6137647090ea4d92e
gyeon <75626815+gyujun9403@users.noreply.github.com>
2023-09-04 19:25:03 +0900

    style: 로거 추가 수정

--- GameServer/GameServer/Logger.cpp
+++ GameServer/GameServer/Logger.cpp
@@ -1,2 +1,55 @@
 #include "pch.h"
 #include "Logger.h"
+
+void Logger::Log(LogLevel level, const std::string& callLocation, const std::string& msg)
+{
+	std::string timeStr = GenTimeStr();
+	std::string logLevelStr = GetLogLevelStr(level);
+
+	std::unique_lock<std::mutex> lock(_queueMutex);
+	waitingLogQueue.push("[" + timeStr + "](" + logLevelStr + ") " + callLocation + " " + msg);
+}
+
+void Logger::PrintLogMsg()
+{
+	std::string msg;
+	std::unique_lock<std::mutex> lock(_queueMutex);
+	while (waitingLogQueue.empty() == false)
+	{
+		writingLogQueue.push(std::move(waitingLogQueue.front()));
+		waitingLogQueue.pop();
+	}
+	lock.unlock();
+	while (writingLogQueue.empty() == false)
+	{
+		std::cout << writingLogQueue.front() << std::endl;
+		writingLogQueue.pop();
+	}
+}
+
+std::string Logger::GenTimeStr()
+{
+	std::time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	struct tm localTime;
+	std::ostringstream oss;
+	localtime_s(&localTime, &currentTime);
+	oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
+
+	return oss.str();
+}
+
+std::string Logger::GetLogLevelStr(LogLevel level)
+{
+	switch (level)
+	{
+	case LogLevel::INFO:
+		return "INFO";
+	case LogLevel::WARNING:
+		return "WARNING";
+	case LogLevel::ERR:
+		return "ERR";
+	case LogLevel::CRITICAL:
+		return "CRITICAL";
+	}
+	return "UNKNOWN";
+}
--- GameServer/GameServer/Logger.h
+++ GameServer/GameServer/Logger.h
@@ -15,31 +15,8 @@ enum class LogLevel : UINT16
 class Logger : public Singleton<Logger>
 {
 public:
-	void Log(LogLevel level, const std::string& callLocation, const std::string& msg)
-	{
-		std::string timeStr = GenTimeStr();
-		std::string logLevelStr = GetLogLevelStr(level);
-
-		std::unique_lock<std::mutex> lock(_queueMutex);
-		waitingLogQueue.push("[" + timeStr + "](" + logLevelStr + ") " + callLocation + " " + msg);
-	}
-
-	void PrintLogMsg()
-	{
-		std::string msg;
-		std::unique_lock<std::mutex> lock(_queueMutex);
-		while (waitingLogQueue.empty() == false)
-		{
-			writingLogQueue.push(std::move(waitingLogQueue.front()));
-			waitingLogQueue.pop();
-		}
-		lock.unlock();
-		while (writingLogQueue.empty() == false)
-		{
-			std::cout << writingLogQueue.front() << std::endl;
-			writingLogQueue.pop();
-		}
-	}
+	void Log(LogLevel level, const std::string& callLocation, const std::string& msg);
+	void PrintLogMsg();
 
 private:
 	std::mutex _queueMutex;
@@ -47,31 +24,7 @@ private:
 	std::queue<std::string> waitingLogQueue;
 	std::queue<std::string> writingLogQueue;
 
-	std::string GenTimeStr()
-	{
-		std::time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		struct tm localTime;
-		std::ostringstream oss;
-		localtime_s(&localTime, &currentTime);
-		oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
-
-		return oss.str();
-	}
-
-	std::string GetLogLevelStr(LogLevel level)
-	{
-		switch (level)
-		{
-		case LogLevel::INFO:
-			return "INFO";
-		case LogLevel::WARNING:
-			return "WARNING";
-		case LogLevel::ERR:
-			return "ERR";
-		case LogLevel::CRITICAL:
-			return "CRITICAL";
-		}
-		return "UNKNOWN";
-	}
+	std::string GenTimeStr();
+	std::string GetLogLevelStr(LogLevel level);
 };
 
